220f7a6dd0dc7a14e85833d2265c16e0f0f264f0
MS\hcardoso <hernan.f.cardoso@gmail.com>
2023-07-16 18:44:13 -0300

    feat. Dashboard and welcome views customized. Welcome now uses some layout that dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,12 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            {{-- <x-welcome /> --}}
-            @php
+            <x-welcome />
+
+            {{--     
+             @php
                 $type = 'danger';
             @endphp
             <x-alert2 :type="$type" class="mb-4">
@@ -18,7 +15,10 @@
                 </x-slot>
                 <p>slot principal</p>
             </x-alert2>
-            hola mundo
+            <x-button>
+                Submit
+            </x-button> --}}
+
 
         </div>
     </div>
